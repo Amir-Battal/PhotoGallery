38c09560f32ff751eeb5f284c95a6412f983a8aa
Amir-Battal <ameerbatttal@hotmail.com>
2024-03-26 19:32:20 +0300

    edit

--- client/src/components/Register/index.jsx
+++ client/src/components/Register/index.jsx
@@ -28,7 +28,7 @@ const Register = () => {
         }
         
         try {
-            const url = "https://photo-gallery-server.vercel.app/api/auth/register";
+            const url = "https://photo-gallery-server-eight.vercel.app/api/auth/register";
             const { data: res } = await axios.post(url, data)
                                             .then(res => {
                                                 Auth.login(res.data);
