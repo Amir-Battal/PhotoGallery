922c5b257a45279bf3479e698203ca7a614f58c2
Amir-Battal <ameerbatttal@hotmail.com>
2024-03-27 13:45:03 +0300

    .

--- client/src/components/InputForrm/index.jsx
+++ client/src/components/InputForrm/index.jsx
@@ -41,17 +41,17 @@ const InputForm = (props) => {
             .catch(err => console.log(err));
     }
 
-    const handleChange = (e) => {
-        const selectedFile = e.target.files[0];
-        if(selectedFile){
-            const allowedFileTypes = ["image/jpeg", "image/jpg", "image/png"];
-            if(!allowedFileTypes.includes(selectedFile.type)){
-                alert("الرجاء اختيار ملف من نوع JPEG أو JPG أو PNG.");
-            } else {
-                setPhoto(selectedFile);
-            }
-        }
-    };
+    // const handleChange = (e) => {
+    //     const selectedFile = e.target.files[0];
+    //     if(selectedFile){
+    //         const allowedFileTypes = ["image/jpeg", "image/jpg", "image/png"];
+    //         if(!allowedFileTypes.includes(selectedFile.type)){
+    //             alert("الرجاء اختيار ملف من نوع JPEG أو JPG أو PNG.");
+    //         } else {
+    //             setPhoto(selectedFile);
+    //         }
+    //     }
+    // };
 
     return (
         <div>
@@ -80,8 +80,8 @@ const InputForm = (props) => {
                     type="file"
                     name="file_picker"
                     id="file_picker"
-                    onChange={handleChange}
-                    required
+                    // onChange={handleChange}
+                    // required
                 />
                 <button className={styles.addPhoto} type="submit">
                     إضافة الصورة
