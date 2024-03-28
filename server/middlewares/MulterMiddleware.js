0a759daf4b58e3ef99f12525e7b4980a40057098
Amir-Battal <ameerbatttal@hotmail.com>
2024-03-28 12:19:13 +0300

    finalEdit

--- server/middlewares/MulterMiddleware.js
+++ server/middlewares/MulterMiddleware.js
@@ -1,9 +1,27 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require('fs');
+
+const uploadPath = path.join(__dirname, '../public', 'uploads');
+
+fs.access(uploadPath, fs.constants.F_OK, (err) => {
+    if (err) {
+        fs.mkdir(uploadPath, { recursive: true }, (err) => {
+            if (err) {
+                console.error('خطأ في إنشاء المجلد:', err);
+            } else {
+                console.log('تم إنشاء المجلد بنجاح.');
+            }
+        });
+    } else {
+        console.log('المجلد موجود بالفعل.');
+    }
+});
+
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, "./public/uploads")
+        cb(null, path.join(__dirname, "../public/uploads"));
     },
     filename: function(req, file, cb) {
         cb(null, Date.now() + path.extname(file.originalname));
